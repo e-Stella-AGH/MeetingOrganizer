b48c85d14c16c6e1ec95785dd73704f384c907c6
Rados13 <radoslawszuma@gmail.com>
2021-07-24 14:44:23 +0200

    Changes in relations.js file

--- db/relations.js
+++ db/relations.js
@@ -4,15 +4,20 @@ const {Guest} = require('./guest')
 const {Host} = require('./host')
 const {TimeSlot} = require('./time_slot')
 
-exports.setRelations = () => { 
-    Organizer.meetings = Organizer.belongsToMany(Meeting, {through: "OrganizerMeeting"})
-    Meeting.organizer = Meeting.belongsTo(Organizer, {through: "OrganizerMeeting"})
+Organizer.meetings = Organizer.belongsToMany(Meeting, {through: "OrganizerMeeting"})
+Meeting.organizer = Meeting.belongsTo(Organizer, {through: "OrganizerMeeting"})
 
-    Meeting.guest = Meeting.hasOne(Guest, {through: "GuestMeeting"})
-    Guest.meetings = Guest.belongsToMany(Meeting, {through: "GuestMeeting"})
+Meeting.guest = Meeting.hasOne(Guest, {through: "GuestMeeting"})
+Guest.meetings = Guest.belongsToMany(Meeting, {through: "GuestMeeting"})
 
-    Meeting.hosts = Meeting.belongsToMany(Host, {through: "MeetingHost"})
-    Host.meetings = Host.belongsToMany(Meeting, {through: "MeetingHost"})
+Meeting.hosts = Meeting.belongsToMany(Host, {through: "MeetingHost"})
+Host.meetings = Host.belongsToMany(Meeting, {through: "MeetingHost"})
 
-    Host.timeSlots = Host.hasMany(TimeSlot)
+Host.timeSlots = Host.hasMany(TimeSlot)
+
+exports.models = {
+    Organizer: Organizer,
+    Meeting: Meeting,
+    Host: Host,
+    Guest: Guest
 }
