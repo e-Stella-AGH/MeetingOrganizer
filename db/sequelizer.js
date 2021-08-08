5e8b43ecccd471c1aa6d5ed310ee718ecc854d1d
Rados13 <radoslawszuma@gmail.com>
2021-08-08 16:28:55 +0200

    Add more sequelize options

--- db/sequelizer.js
+++ db/sequelizer.js
@@ -1,7 +1,20 @@
 const { Sequelize } = require('sequelize')
 const env = process.env
-const dbUri = "DATABASE_URL" in env ? env.DATABASE_URL : 'sqlite::memory:'
-const sequelize = new Sequelize(dbUri, { logging: false, dialectOptions: { ssl: true } })
+const isPostgres = "DATABASE_URL" in env
+const dbUri = isPostgres ? env.DATABASE_URL : 'sqlite::memory:'
+const isPostgres = true
+const options = isPostgres ? {
+    dialect: "postgres",
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+} : {}
+
+const sequelize = new Sequelize(dbUri, { logging: false, ...options })
 
 
 exports.sequelize = sequelize
