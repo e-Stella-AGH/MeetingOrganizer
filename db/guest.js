b81ee234ae366b9a1d73af3a3def7519a5e84f26
Rados13 <radoslawszuma@gmail.com>
2021-09-17 20:19:59 +0200

    Change guest model

--- db/guest.js
+++ db/guest.js
@@ -1,9 +1,9 @@
-const {sequelize} = require("./sequelizer")
-const {DataTypes } = require("sequelize");
+const { sequelize } = require("./sequelizer")
+const { DataTypes } = require("sequelize");
 
 
-const Guest = sequelize.define("Guests", {
-    email: {type: DataTypes.STRING, primaryKey: true}
+const Guest = sequelize.define("Guest", {
+    email: { type: DataTypes.STRING, primaryKey: true }
 }, { timestamps: false })
 
 exports.Guest = Guest
